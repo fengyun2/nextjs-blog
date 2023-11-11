e8336e598094864952c630f8cb6053519af782b6
ly2011 <1518550424@qq.com>
2023-11-11 11:28:49 +0800

    chore: adjust posts dir

--- src/app/components/ListItem.tsx
+++ src/app/components/ListItem.tsx
@@ -10,7 +10,7 @@ export default function ListItem({ post }: Props) {
   const formattedDate = getFormattedDate(date);
   return (
     <li className='mt-4 text-2xl dark:text-white/90'>
-      <Link className='underline hover:text-black/70 dark:hover:text-white' href={`/posts/${id}`}>
+      <Link className='underline hover:text-black/70 dark:hover:text-white' href={`/posts/detail/${id}`}>
         {title}
       </Link>
       <br />
--- src/app/posts/add/page.tsx
+++ src/app/posts/add/page.tsx
@@ -1,16 +1,28 @@
 'use client';
 
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-export function AddPost() {
-  const [title, setTitle] = useState('');
+import dynamic from 'next/dynamic';
+import { Suspense, useRef } from 'react';
+import type { MDXEditorMethods } from '@mdxeditor/editor';
+
+const MDXEditorComp = dynamic(() => import('../../components/MDXEditorComponent'), {
+  ssr: false,
+});
+
+const markdown = `
+# Hello world!
+Check the EditorComponent.tsx file for the code .
+Hello [world](https://virtuoso.dev/)
+`;
+
+export default function AddPost() {
+  const ref = useRef<MDXEditorMethods>(null);
   return (
-    <div className='detail'>
-      <div className='detail-header'>
-        <div className='detail-header-title'>
-          <h1>Add Post</h1>
-        </div>
-      </div>
-    </div>
+    <>
+      <button onClick={() => ref.current?.setMarkdown('new markdown')}>Set new markdown</button>
+      <button onClick={() => console.log(ref.current?.getMarkdown())}>Get markdown</button>
+      <Suspense fallback={<div>Loading...</div>}>
+        <MDXEditorComp editorRef={ref} markdown={markdown} />
+      </Suspense>
+    </>
   );
 }
