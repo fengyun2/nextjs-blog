b45c7acc111ffb02eb5bdc16707462aabbcaee78
ly2011 <1518550424@qq.com>
2023-11-11 15:44:31 +0800

    feat: add code blocks

--- src/app/components/MDXEditorComponent.tsx
+++ src/app/components/MDXEditorComponent.tsx
@@ -2,16 +2,42 @@
 
 import '@mdxeditor/editor/style.css';
 import type { ForwardedRef } from 'react';
-// import {
-//   headingsPlugin,
-//   listsPlugin,
-//   quotePlugin,
-//   thematicBreakPlugin,
-//   markdownShortcutPlugin,
-//   MDXEditor,
-//   type MDXEditorMethods,
-//   type MDXEditorProps,
-// } from '@mdxeditor/editor';
+import {
+  // headingsPlugin,
+  // listsPlugin,
+  // quotePlugin,
+  // thematicBreakPlugin,
+  // markdownShortcutPlugin,
+  // MDXEditor,
+  // type MDXEditorMethods,
+  // type MDXEditorProps,
+
+  Separator,
+  // CodeToggle,
+  // InsertCodeBlock,
+  InsertSandpack,
+  // ConditionalContents,
+  // ShowSandpackInfo,
+  // InsertFrontmatter,
+  // ChangeCodeMirrorLanguage,
+  // TooltipWrap,
+  // Button,
+  // BlockTypeSelect,
+  // InsertThematicBreak,
+  // InsertAdmonition,
+  // CreateLink,
+  // ListsToggle,
+  // ChangeAdmonitionType,
+  frontmatterPlugin,
+  diffSourcePlugin,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  sandpackPlugin,
+  KitchenSinkToolbar,
+  type SandpackConfig,
+  type EditorInFocus,
+  type AdmonitionKind,
+} from '@mdxeditor/editor';
 
 import { MDXEditor, type MDXEditorMethods, type MDXEditorProps } from '@mdxeditor/editor/MDXEditor';
 import { headingsPlugin } from '@mdxeditor/editor/plugins/headings';
@@ -20,16 +46,94 @@ import { quotePlugin } from '@mdxeditor/editor/plugins/quote';
 import { thematicBreakPlugin } from '@mdxeditor/editor/plugins/thematic-break';
 import { markdownShortcutPlugin } from '@mdxeditor/editor/plugins/markdown-shortcut';
 import { linkPlugin } from '@mdxeditor/editor/plugins/link';
-import { linkDialogPlugin } from '@mdxeditor/editor/plugins/link-dialog';
+// import { linkDialogPlugin } from '@mdxeditor/editor/plugins/link-dialog';
 import { imagePlugin } from '@mdxeditor/editor/plugins/image';
-import { UndoRedo } from '@mdxeditor/editor/plugins/toolbar/components/UndoRedo';
-import { BoldItalicUnderlineToggles } from '@mdxeditor/editor/plugins/toolbar/components/BoldItalicUnderlineToggles';
-import { InsertImage } from '@mdxeditor/editor/plugins/toolbar/components/InsertImage';
+import { tablePlugin } from '@mdxeditor/editor/plugins/table';
+// import { codeBlockPlugin } from '@mdxeditor/editor/plugins/codeblock';
+// import { codeMirrorPlugin } from '@mdxeditor/editor/plugins/codemirror';
+// import { UndoRedo } from '@mdxeditor/editor/plugins/toolbar/components/UndoRedo';
+// import { BoldItalicUnderlineToggles } from '@mdxeditor/editor/plugins/toolbar/components/BoldItalicUnderlineToggles';
+// import { InsertImage } from '@mdxeditor/editor/plugins/toolbar/components/InsertImage';
+// import { InsertTable } from '@mdxeditor/editor/plugins/toolbar/components/InsertTable';
 import { toolbarPlugin } from '@mdxeditor/editor/plugins/toolbar';
+
+import dataCode from 'raw-loader!../assets/dataCode.ts';
 
 type EditorProps = {
   editorRef?: ForwardedRef<MDXEditorMethods> | null;
 } & MDXEditorProps;
+
+const defaultSnippetContent = `
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+`.trim();
+
+export const virtuosoSampleSandpackConfig: SandpackConfig = {
+  defaultPreset: 'react',
+  presets: [
+    {
+      label: 'React',
+      name: 'react',
+      meta: 'live react',
+      sandpackTemplate: 'react',
+      sandpackTheme: 'light',
+      snippetFileName: '/App.js',
+      snippetLanguage: 'jsx',
+      initialSnippetContent: defaultSnippetContent,
+    },
+    {
+      label: 'React',
+      name: 'react',
+      meta: 'live',
+      sandpackTemplate: 'react',
+      sandpackTheme: 'light',
+      snippetFileName: '/App.js',
+      snippetLanguage: 'jsx',
+      initialSnippetContent: defaultSnippetContent,
+    },
+    {
+      label: 'Vanilla',
+      name: 'vanilla',
+      meta: 'live vanilla',
+      sandpackTheme: 'auto',
+      sandpackTemplate: 'vanilla',
+      snippetFileName: '/index.js',
+      snippetLanguage: 'js',
+      initialSnippetContent: `console.log('Hello world')`,
+    },
+    {
+      label: 'Virtuoso',
+      name: 'virtuoso',
+      meta: 'live virtuoso',
+      sandpackTemplate: 'react-ts',
+      sandpackTheme: 'light',
+      snippetFileName: '/App.tsx',
+      initialSnippetContent: defaultSnippetContent,
+      dependencies: {
+        'react-virtuoso': 'latest',
+        '@ngneat/falso': 'latest',
+      },
+      files: {
+        // @ts-ignore
+        '/data.ts': dataCode,
+      },
+    },
+  ],
+};
+
+function whenInAdmonition(editorInFocus: EditorInFocus | null) {
+  const node = editorInFocus?.rootNode;
+  if (!node || node.getType() !== 'directive') {
+    return false;
+  }
+  return ['note', 'tip', 'danger', 'info', 'caution'].includes(node.getMdastNode().name as AdmonitionKind);
+}
 
 async function imageUploadHandler(image: File) {
   const formData = new FormData();
@@ -49,6 +153,7 @@ async function imageUploadHandler(image: File) {
 export default function MDXEditorComponent({ editorRef, ...props }: EditorProps) {
   return (
     <MDXEditor
+      contentEditableClassName='prose max-w-none'
       {...props}
       plugins={[
         headingsPlugin(),
@@ -57,7 +162,7 @@ export default function MDXEditorComponent({ editorRef, ...props }: EditorProps)
         thematicBreakPlugin(),
         markdownShortcutPlugin(),
         linkPlugin(),
-        linkDialogPlugin(),
+        // linkDialogPlugin(),
         imagePlugin({
           imageUploadHandler,
           // imagePreviewHandler: () => {
@@ -65,14 +170,92 @@ export default function MDXEditorComponent({ editorRef, ...props }: EditorProps)
           // },
           // imageAutocompleteSuggestions: ['https://picsum.photos/200/300', 'https://picsum.photos/200'],
         }),
+        tablePlugin(),
+        frontmatterPlugin(),
+        diffSourcePlugin({ viewMode: 'rich-text', diffMarkdown: 'boo' }),
+        codeBlockPlugin({ defaultCodeBlockLanguage: 'js' }),
+        codeMirrorPlugin({ codeBlockLanguages: { js: 'JavaScript', css: 'CSS', txt: 'text', tsx: 'TypeScript' } }),
+        sandpackPlugin({ sandpackConfig: virtuosoSampleSandpackConfig }),
         toolbarPlugin({
           toolbarContents: () => (
             <>
-              <UndoRedo />
-              <BoldItalicUnderlineToggles />
-              <InsertImage />
+              <InsertSandpack />
+              <Separator />
+
+              <KitchenSinkToolbar />
             </>
           ),
+          // toolbarContents: () => (
+          //   <>
+          //     <ConditionalContents
+          //       options={[
+          //         {
+          //           when: (editor) => editor?.editorType === 'codeblock',
+          //           contents: () => <ChangeCodeMirrorLanguage />,
+          //         },
+          //         { when: (editor) => editor?.editorType === 'sandpack', contents: () => <ShowSandpackInfo /> },
+          //         {
+          //           fallback: () => (
+          //             <>
+          //               <TooltipWrap title='Save to database'>
+          //                 <div className='h-[32px] w-[29px]'>
+          //                   <Button onClick={() => console.log('保存成功')}>💾</Button>
+          //                 </div>
+          //               </TooltipWrap>
+          //               <UndoRedo />
+          //               <Separator />
+          //               <BoldItalicUnderlineToggles />
+          //               <CodeToggle />
+          //               <Separator />
+          //               <ListsToggle />
+          //               <Separator />
+
+          //               <ConditionalContents
+          //                 options={[
+          //                   { when: whenInAdmonition, contents: () => <ChangeAdmonitionType /> },
+          //                   { fallback: () => <BlockTypeSelect /> },
+          //                 ]}
+          //               />
+
+          //               <Separator />
+
+          //               <CreateLink />
+          //               <InsertImage />
+
+          //               <Separator />
+
+          //               <InsertTable />
+          //               <InsertThematicBreak />
+
+          //               <Separator />
+          //               <InsertCodeBlock />
+
+          //               <Separator />
+          //               <InsertSandpack />
+
+          //               <ConditionalContents
+          //                 options={[
+          //                   {
+          //                     when: (editorInFocus) => !whenInAdmonition(editorInFocus),
+          //                     contents: () => (
+          //                       <>
+          //                         <Separator />
+          //                         <InsertAdmonition />
+          //                       </>
+          //                     ),
+          //                   },
+          //                 ]}
+          //               />
+
+          //               <Separator />
+          //               <InsertFrontmatter />
+          //             </>
+          //           ),
+          //         },
+          //       ]}
+          //     />
+          //   </>
+          // ),
         }),
       ]}
       ref={editorRef}
--- src/app/posts/add/page.tsx
+++ src/app/posts/add/page.tsx
@@ -12,14 +12,43 @@ const markdown = `
 # Hello world!
 Check the EditorComponent.tsx file for the code .
 Hello [world](https://virtuoso.dev/)
+
+## Table
+
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | 600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
+
+
+Image without dimensions:
+
+![](https://images.unsplash.com/photo-1517935706615-2717063c2225)
+
+Image with dimensions:
+
+<img src="https://images.unsplash.com/photo-1517935706615-2717063c2225" width="500" height="auto" />
 `;
 
 export default function AddPost() {
   const ref = useRef<MDXEditorMethods>(null);
   return (
     <>
-      <button onClick={() => ref.current?.setMarkdown('new markdown')}>Set new markdown</button>
-      <button onClick={() => console.log(ref.current?.getMarkdown())}>Get markdown</button>
+      <button
+        type='button'
+        className='rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
+        onClick={() => ref.current?.setMarkdown('new markdown')}
+      >
+        Set new markdown
+      </button>
+      <button
+        type='button'
+        className='inline-flex items-center rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+        onClick={() => console.log(ref.current?.getMarkdown())}
+      >
+        Get markdown
+      </button>
       <Suspense fallback={<div>Loading...</div>}>
         <MDXEditorComp editorRef={ref} markdown={markdown} />
       </Suspense>
